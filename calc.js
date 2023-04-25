057cadd79ab66e204d1744a31c9599cd028ab059
DokZokiu <enzo.rosier@student.junia.com>
2023-04-25 15:27:33 +0200

    etape 3

--- calc.js
+++ calc.js
@@ -1,4 +1,3 @@
-
 var calculette;
 
 window.onload = function () {
@@ -18,11 +17,11 @@ class Calculette {
         this.screen = screen;
         this.formule = [""]; //Liste contenant les opérateurs et opérandes
         this.cancel_list = [];
+        this.timeStart = -1;
     }
 
     //Fonction qui met à jour l'affichage
     refreshScreens() {
-
         let resScreen = ""; //Expression à affiché
         this.formule.forEach((val) => {
             // Pour chaque opérateur/opérande
@@ -66,6 +65,9 @@ class Calculette {
 
     //Fonction qui rajoute un symbole
     AddSymbole(symb) {
+        if (this.timeStart == -1) {
+            this.timeStart = performance.now();
+        }
         this.cancel_list.push(this.formule.slice()); //Copie de l'array
         this.formule.push(symb);
         this.refreshScreens();
@@ -73,7 +75,8 @@ class Calculette {
 
     //Fonction qui annule la dernière action effectue
     Cancel() {
-        if (this.cancel_list.length > 0) { //Si des actions ont été faites
+        if (this.cancel_list.length > 0) {
+            //Si des actions ont été faites
             this.formuleScreen.innerHTML = "";
             this.resultScreen.innerHTML = "";
             this.formule = this.cancel_list.pop();
@@ -83,6 +86,8 @@ class Calculette {
 
     //Fonction qui calcule la formule
     calculate() {
+        
+        
         this.cancel_list.push(this.formule.slice());
         this.formuleScreen.innerHTML = this.screen.innerHTML; //On écrit la formule en haut a gauche de la calculette
         let expression = ""; //On initialise l'expression
@@ -92,10 +97,51 @@ class Calculette {
 
         try {
             this.resultScreen.innerHTML = eval(expression);
+            let timeTaken = 0
+            if (this.timeStart != -1) {
+                timeTaken =  performance.now() - this.timeStart;
+                this.timeStart = -1;
+                this.sendSuccessRequest(timeTaken);
+            }
         } catch (err) {
             // On essaye d'afficher le résultat
             //Si une erreur survient
             this.resultScreen.innerHTML = "ERREUR"; //On affiche ERREUR à la place du résultat
+            this.sendFailRequest()
         }
     }
+
+    sendFailRequest() {
+        console.log("Send fail request");
+        let url = "http://localhost:3000/fail";
+        let dataraw = {};
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", url, true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send(JSON.stringify(dataraw));
+
+        xhttp.onload = function () {
+            console.log(this.responseText);
+            let res = JSON.parse(this.responseText)
+            alert('Date de la dernière erreur : ' + res[0]['created_at'] + '\nNombre d\'érreur : ' + res[1])
+        };
+    }
+
+    sendSuccessRequest(time) {
+        console.log("Send success request");
+        let url = "http://localhost:3000/success";
+        let dataraw = {timeTakenMs: time};
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", url, true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send(JSON.stringify(dataraw));
+
+        xhttp.onload = function () {
+            console.log(this.responseText);
+            let res = JSON.parse(this.responseText)
+            alert('Temps pris : ' + time.toFixed(2) + ' ms' + '\nTemps moyen : ' + res[0].toFixed(2) + ' ms\nPlus rapide que ' + res[1].toFixed(2) + '%')
+        };
+    }
 }
+
+
